0a231458423db39afddd59b8c8c287d60982f272
adeennour4-dot <adeennour4@gmail.com>
2025-09-26 01:33:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,15 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // This section directly configures PostCSS within Vite
-  // It ensures tailwindcss runs first, which fixes the error.
   css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
+    transformer: 'postcss'
   },
-  
-  // This section fixes the "@" path shortcut error
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
+    }
+  }
 })
