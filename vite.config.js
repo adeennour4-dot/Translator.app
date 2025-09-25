397e8f3c9f33be53a779d58149c8ff76df500c8f
adeennour4-dot <adeennour4@gmail.com>
2025-09-26 01:20:55 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,11 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // This section fixes the "lightningcss" error by telling Vite
-  // to use a more compatible CSS processor.
   css: {
     transformer: 'postcss'
   },
-
-  // This section fixes the "@" path shortcut error so Vite
-  // can find your components.
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
