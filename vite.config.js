d8da02b4a72d2b4f066f2aa7f9a8b1eedd556941
adeennour4-dot <adeennour4@gmail.com>
2025-09-26 01:16:06 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This section fixes the CSS build issue you had in Termux
+
+  // This section fixes the "lightningcss" error by telling Vite
+  // to use a more compatible CSS processor.
   css: {
     transformer: 'postcss'
   },
-  // This section fixes the "@" path shortcut error
+
+  // This section fixes the "@" path shortcut error so Vite
+  // can find your components.
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
