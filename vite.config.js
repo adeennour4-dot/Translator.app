d1eba5a5a0e87887127033dee6b53f17c55f1efe
adeennour4-dot <adeennour4@gmail.com>
2025-09-25 19:46:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,25 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        // Mark Capacitor plugins as external so Rollup doesn't try to bundle them
+        '@capacitor/app',
+        '@capacitor/haptics',
+        '@capacitor/keyboard',
+        '@capacitor/status-bar',
+        '@capacitor/filesystem', // <-- Add this line
+        // Add any other Capacitor plugins you might be using here
+      ],
     },
   },
 })
