2eff769b604ade135ea8afb920a9d2c8710bde3d
adeennour4-dot <adeennour4@gmail.com>
2025-09-25 22:52:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,14 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { fileURLToPath } from 'url'
 
-// __dirname replacement for ESM
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: './', // required for Capacitor offline bundle
-  build: {
-    outDir: 'dist',
-  },
-=======
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    transformer: 'postcss'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  }
 })
-
