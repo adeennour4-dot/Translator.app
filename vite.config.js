be50adf1d3f5758fadeb73941388c5d9b99e80d5
adeennour4-dot <adeennour4@gmail.com>
2025-09-27 01:06:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- Import the official v4 plugin
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss(), // <-- Use the plugin here
+    tailwindcss(),
   ],
   resolve: {
     alias: {
